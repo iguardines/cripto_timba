f03ab143a522d3fd30fb4d5ddf7e97698ebe7142
iguardines <iguardines@gmail.com>
2021-07-16 21:18:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/cripto_timba')
-def hello_world():
-   return 'Hola cripto timbero'
+DEBUG = True
+timberes = ["Nacho", "Cris", "Valen"]
+@app.errorhandler(404)
+def not_found(error):
+	return "Not Found"
 
-if __name__ == '__main__':
-   app.run()
+
+@app.route('/agregar_timbero', methods=['POST'])
+def index():
+    timbero = request.args.get("timbero", "nadie")
+    timberes.append(timbero)
+    return f"timbero {timbero} agregado safisfactoriamente"
+
+
+@app.route('/la_persona_criptotimbera_del_mes', methods=['GET'])
+def timbier_of_the_month():
+    return random.choice(timberes)
+
+@app.route('/criptotimberes', methods=['GET'])
+def criptotimberes():
+    return {"timberos" :timberes}
+
+if __name__ == "__main__":
+	app.run(port=5000, debug= DEBUG)
